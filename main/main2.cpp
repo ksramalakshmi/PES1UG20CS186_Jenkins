af16f97a467cbbe2275fbbabe9ca5582a231b45a
K S Ramalakshmi <rlsri2305@gmail.com>
2023-02-19 11:19:56 +0530

    Delete main2.cpp

--- main/main2.cpp
+++
@@ -1,9 +0,0 @@
-//File: hello.cpp
-#include <iostream>
-using namespace std;
-
-int main(void) {
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully created a new file and pushed\n";
-  return 0;
-}
